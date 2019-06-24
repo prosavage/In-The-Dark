ba008dc9581f50f4f3aef74fac76898bf47139eb
ProSavage <naman0311@gmail.com>
2019-06-24 09:50:57 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/_main.scss';
+
+function App() {
+  return (
+    <div className="main-container">
+
+    </div>
+  );
+}
+
+export default App;
