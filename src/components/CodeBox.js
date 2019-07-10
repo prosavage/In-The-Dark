17779d43f52e06f3290f3f6ecce462a8f4191f41
ProSavage <naman0311@gmail.com>
2019-07-10 00:01:58 -0500

    FIxed tabs, removed prism highlighting

--- src/components/CodeBox.js
+++ src/components/CodeBox.js
@@ -1,28 +1,34 @@
 import React, {Component} from 'react';
-import Prism from 'prismjs'
 import "./../styles/prism.css"
 import {htmlSnippet, cssSnippet} from "./../Snippets"
 
+
+
 class CodeBox extends Component {
 
-    componentDidMount() {
-        Prism.highlightAll();
-    }
 
-    processInput(e) {
-        console.log(e);
-        var el = document.getElementById(this.props.language);
-        var range = document.createRange();
-        var sel = window.getSelection();
-        Prism.highlightAll();
-        window.document.s
-    }
+    processTabs = (e) => {
+        const keyCode = e.keyCode || e.which;
+        if (keyCode === 9) {
+            const textarea = this.textarearef;
+            e.preventDefault();
+            const start = textarea.selectionStart;
+            const end = textarea.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            textarea.value = (textarea.value.substring(0, start) + "\t" + textarea.value.substring(end));
+
+            // put caret at right position again
+            textarea.selectionStart = textarea.selectionEnd = start + 1;
+        }
+    };
+
 
     render() {
         return (
             <div className={"code-box-container"}>
                 <h3>{this.props.language.toUpperCase()}</h3>
-               <pre id={this.props.language} onInput={(e) => this.processInput(e)} contentEditable={true}>{this.props.language === "html" ? htmlSnippet : cssSnippet}</pre>
+               <textarea ref={(ref) => {this.textarearef = ref}} id={this.props.language} onKeyDown={(e) => this.processTabs(e)} contentEditable={true}>{this.props.language === "html" ? htmlSnippet : cssSnippet}</textarea>
             </div>
 
         );
